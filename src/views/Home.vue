91672837f5adb15aa2cd2100e4ba2945c5179637
motoki317 <motoki317@gmail.com>
2019-05-20 16:00:27 +0900

    Added basic site

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+      integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+      crossorigin="anonymous"
+    />
+
+    <img alt="icon" src="../assets/icon.png" class="icon" />
+    <h1>motoki1</h1>
+    <h2>制作物とかをまとめるところ</h2>
+    <div class="introduction">
+      <h3>自己紹介をちょっと</h3>
+      <p>
+        <b>名前</b>
+        <br />motoki1 または toki
+        <br />
+        <b>所属</b>
+        <br />titech - デジタル創作同好会traP（2019~）
+      </p>
+      <p>
+        <b>趣味</b>
+        <br />プログラミング,アニメ（鑑賞）, けものフレンズ etc.
+        <br />
+        <b>プログラミング</b>
+        <br />Java, Go, Vue.js, HTML, CSS etc. <br />メインはJava
+      </p>
+    </div>
+    <div class="footer">
+      <b>その他</b>
+      <br />
+      <span class="fab fa-twitter twitter"></span>
+      <a href="https://twitter.com/motoki1_">motoki1_</a>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+<style>
+.icon {
+  width: 100px;
+  border-radius: 100%;
+}
+
+.introduction {
+  margin-top: 50px;
+}
+
+.footer {
+  margin-top: 20px;
+}
+
+.twitter {
+  color: #55acee;
+}
+</style>
---
+++ src/views/Works.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="works">
+    <h1>My Works</h1>
+    <h3>作ったもの</h3>
+
+    <div class="work-list">
+      <div>
+        <h2>2018</h2>
+        <b>moto-bot （Discord bot）</b>
+        <p>
+          <a
+            href="https://forums.wynncraft.com/threads/discord-bot-for-wynn-utility-commands-moto-bot.223425/"
+            >Botについて</a
+          >
+          -
+          <a
+            href="https://discordapp.com/oauth2/authorize?client_id=418413540857085972&scope=bot&permissions=93248"
+            >Invite Link</a
+          >
+          <br />
+          <img src="../assets/images/moto-bot.png" alt="moto-bot" />
+          <br />DiscordのBotを書いた - 初めてのプログラミングをした。
+          <br />言語はJava, ライブラリはJDA（Java Discord
+          API）を使用。主にWynncraft（Minecraftのサーバー）のAPIを使って便利そうな機能を
+          追加しまくったらいつの間にか肥大化していた。 <br />音楽とか聞けるよ！
+        </p>
+      </div>
+      <h2>2019</h2>
+      <b>簡単なフロントエンド</b>
+      <p>
+        traPの講習会で教えてもらったVue.jsを使い簡単なサイトを作ってみる。
+        <br />（Vue.jsのRouter初期機能を使っているだけ） <br />作ったもの
+        <br />
+        <a href="https://wynn-ingreds-searcher.netlify.com/"
+          >Wynn Ingredients Searcher</a
+        >
+        - 実用的（にはほど遠い）
+        <br />
+        <img
+          src="../assets/images/wynn-ingreds-searcher.png"
+          alt="wynn-ingreds-searcher"
+          class="wynn-ingreds-searcher"
+        />
+        <br />
+        <br />
+        <b>このサイト</b>
+        <br />Self-explanatory - 生やした。
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Works"
+};
+</script>
+
+<style>
+.work-list {
+  width: 720px;
+  margin: auto;
+}
+
+.wynn-ingreds-searcher {
+  margin: 5px;
+  border: 1px solid #eee;
+}
+</style>
